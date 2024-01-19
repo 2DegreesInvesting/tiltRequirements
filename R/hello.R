d2815654e0a5056d8456a27e1233c71fd1330965
Mauro Lepore <maurolepore@gmail.com>
2024-01-19 14:40:41 +0000

    Remove shiny

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,4 @@
-#' Greet the world or tilt with code or your mouse
-#'
-#' Greet the world from R with `hello()` or from the web-browser with
-#' `hello_addin()` (from RStudio click on "Addins" and type "hello").
+#' Say hello
 #'
 #' @param who A valid string.
 #'
@@ -17,27 +14,5 @@
 #' try(hello("invalid"))
 hello <- function(who = c("world", "tilt")) {
   who <- match.arg(who)
-  paste("hello", who)
-}
-
-#' @export
-#' @rdname hello
-hello_addin <- function() {
-  ui <- shiny::fluidPage(
-    shiny::selectInput(
-      "who",
-      "Who would you like to greet?",
-      choices = c("world", "tilt"),
-      selected = "world"
-    ),
-    shiny::textOutput("greeting")
-  )
-
-  server <- function(input, output, session) {
-    output$greeting <- shiny::renderText({
-      hello(input$who)
-    })
-  }
-
-  shiny::shinyApp(ui, server)
+  glue::glue("hello {who}")
 }
